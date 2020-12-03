b48d64a1e7f7e3aadb057a465f91ba1e16113f8a
Erik O'Hara <slicedblue@gmail.com>
2020-12-02 21:39:07 -0500

    fixed throw

--- src/main/java/com/example/assignment3/MainController.java
+++ src/main/java/com/example/assignment3/MainController.java
@@ -71,9 +71,7 @@ public class MainController {
 
 		Response response = client.newCall(request).execute();
 		
-		JSONParser p = new JSONParser();
-		Object data = p.parse(response.body().string());
-		JSONObject json = (JSONObject)data;
+		JSONObject json = new JSONObject(response.body().string());
 		JSONArray array = (JSONArray) json.get("data");
 		JSONObject list = (JSONObject) array.get(0);
 		String temperature = (String) list.get("temp");
